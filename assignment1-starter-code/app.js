a80083f961f36b2b27c0490f7216178fc996f806
Tamas Dober <tamas.dober@mobileforming.com>
2016-12-01 00:53:58 -0800

    adding starter files

---
+++ assignment1-starter-code/app.js
@@ -0,0 +1,14 @@
+(function functionName() {
+
+  'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', function ($scope) {
+  $scope.listOfDishes =['soup','fish','salat'];
+  $scope.name="Tamas";
+  console.log(name);
+});
+
+
+
+})();
